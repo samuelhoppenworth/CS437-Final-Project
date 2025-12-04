13d3b9e5ac88b289ff90c0ed28c917f52d41fefb
samuelhoppenworth <shopp7@illinois.edu>
2025-12-04 07:42:28 +0000

    initial commit

---
+++ sample_code_for_RaspberryPi/TS1472_TS1473.c
@@ -0,0 +1,20 @@
+#include <wiringPi.h>
+#include <stdio.h>
+int main()
+{
+  wiringPiSetup();
+  char val;
+  {
+    pinMode(1,INPUT);
+  }
+  
+  while(1) { 
+    val = digitalRead(1);
+    if( val==1 ){
+      printf("There are liquid!\n");
+    } 
+    else {
+      printf("No liquid!\n");
+    }	
+  }
+}
